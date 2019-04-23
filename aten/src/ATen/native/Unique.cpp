youqiangcn/pytorch

Step 6: Rename _unique2 to unique and add int? dim
- Rename `_unique2` to `unique`
- Add optional `dim` argument to make it looks like the signature of Python's `torch.unique`.
- Inside `torch.unique`, use `unique` and get rid of `unique_dim`.
- Unbind `unique_dim` totally from Python at codegen.
- Add OSS ONNX test for unique
- Add jit test for unique

Previously tried in #17097 and cause internal error, not sure about this
time.

cc: @wanchaol

gh-metadata: pytorch pytorch 18655 gh/zasdfgbnm/6/head

aten/src/ATen/native/Unique.cpp
@@ -150,17 +150,20 @@ _unique_cpu(const Tensor& self, const bool sorted, const bool return_inverse) {
 }
 
 std::tuple<Tensor, Tensor, Tensor>
-_unique2_cpu(const Tensor& self, const bool sorted, const bool return_inverse, const bool return_counts) {
-  return AT_DISPATCH_ALL_TYPES(self.scalar_type(), "unique", [&] {
-    return _unique_cpu_template<scalar_t>(self, sorted, return_inverse, return_counts);
-  });
-}
-
-std::tuple<Tensor, Tensor, Tensor>
 unique_dim_cpu(const Tensor& self, const int64_t dim, const bool sorted, const bool return_inverse, const bool return_counts) {
   return AT_DISPATCH_ALL_TYPES(self.scalar_type(), "unique_dim", [&] {
     // The current implementation using `dim` always sorts due to unhashable tensors
     return _unique_dim_cpu_template<scalar_t>(self, dim, return_inverse, return_counts);
+  });
+}
+
+std::tuple<Tensor, Tensor, Tensor>
+unique_cpu(const Tensor& self, const bool sorted, const bool return_inverse, const bool return_counts, c10::optional<int64_t> dim) {
+  if (dim.has_value()) {
+    return unique_dim_cpu(self, dim.value(), sorted, return_inverse, return_counts);
+  }
+  return AT_DISPATCH_ALL_TYPES(self.scalar_type(), "unique", [&] {
+    return _unique_cpu_template<scalar_t>(self, sorted, return_inverse, return_counts);
   });
 }
 

WeiHao97/pytorch_sparse

aten/src/ATen/native/Unique.cpp
@@ -222,13 +222,6 @@ _unique_cpu(const Tensor& self, const bool sorted, const bool return_inverse) {
 }
 
 std::tuple<Tensor, Tensor, Tensor>
-_unique2_cpu(const Tensor& self, const bool sorted, const bool return_inverse, const bool return_counts) {
-  return AT_DISPATCH_ALL_TYPES(self.scalar_type(), "unique", [&] {
-    return unique_cpu_template<scalar_t>(self, sorted, return_inverse, return_counts);
-  });
-}
-
-std::tuple<Tensor, Tensor, Tensor>
 unique_dim_cpu(const Tensor& self, const int64_t dim, const bool sorted, const bool return_inverse, const bool return_counts) {
   return AT_DISPATCH_ALL_TYPES(self.scalar_type(), "unique_dim", [&] {
     // The current implementation using `dim` always sorts due to unhashable tensors
@@ -251,6 +244,16 @@ unique_consecutive_cpu(const Tensor& self, const bool return_inverse, const bool
     });
   }
   return unique_dim_consecutive_cpu(self, dim.value(), return_inverse, return_counts);
+}
+
+std::tuple<Tensor, Tensor, Tensor>
+unique_cpu(const Tensor& self, const bool sorted, const bool return_inverse, const bool return_counts, c10::optional<int64_t> dim) {
+  if (dim.has_value()) {
+    return unique_dim_cpu(self, dim.value(), sorted, return_inverse, return_counts);
+  }
+  return AT_DISPATCH_ALL_TYPES(self.scalar_type(), "unique", [&] {
+    return unique_cpu_template<scalar_t>(self, sorted, return_inverse, return_counts);
+  });
 }
 
 }  // namespace native