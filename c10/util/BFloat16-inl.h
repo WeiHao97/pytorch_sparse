WyldeCat/pytorch

Added Bfloat16 tensor for cpu with very limited support
gh-metadata: pytorch pytorch 21860 gh/izdeby/10/head

c10/util/BFloat16-inl.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <c10/macros/Macros.h>
+#include <limits>
 
 namespace c10 {
 
@@ -17,3 +18,21 @@ inline BFloat16::operator float() const {
 }
 
 } // namespace c10
+
+namespace std {
+
+template <>
+class numeric_limits<c10::BFloat16> {
+  public:
+    static constexpr bool is_signed = true;
+    static constexpr bool has_infinity = true;
+    static constexpr bool has_quiet_NaN = true;
+    static constexpr c10::BFloat16 lowest() {
+      return at::BFloat16(0xFBFF, at::BFloat16::from_bits());
+    }
+    static constexpr c10::BFloat16 max() {
+      return at::BFloat16(0x7BFF, at::BFloat16::from_bits());
+    }
+};
+
+} // namespace std

WeiHao97/pytorch_sparse

c10/util/BFloat16-inl.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <c10/macros/Macros.h>
+#include <limits>
 
 namespace c10 {
 
@@ -15,3 +16,21 @@ inline C10_HOST_DEVICE BFloat16::operator float() const {
 }
 
 } // namespace c10
+
+namespace std {
+
+template <>
+class numeric_limits<c10::BFloat16> {
+  public:
+    static constexpr bool is_signed = true;
+    static constexpr bool has_infinity = true;
+    static constexpr bool has_quiet_NaN = true;
+    static constexpr c10::BFloat16 lowest() {
+      return at::BFloat16(0x0080, at::BFloat16::from_bits());
+    }
+    static constexpr c10::BFloat16 max() {
+      return at::BFloat16(0x7F7F, at::BFloat16::from_bits());
+    }
+};
+
+} // namespace std