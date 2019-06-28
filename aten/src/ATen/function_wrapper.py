malvika2147/pytorch

Enabled BFloat16 storage
gh-metadata: pytorch pytorch 21523 gh/izdeby/9/head

aten/src/ATen/function_wrapper.py
@@ -206,6 +206,7 @@ scalar_types = [
     ('Long', 'int64_t', 'Long', False),
     ('Short', 'int16_t', 'Long', False),
     ('Half', 'Half', 'Double', True),
+    ('BFloat16', 'BFloat16', 'Double', True),
 ]
 
 

WeiHao97/pytorch_sparse

aten/src/ATen/function_wrapper.py
@@ -209,6 +209,7 @@ scalar_types = [
     ('Long', 'int64_t', 'Long', False),
     ('Short', 'int16_t', 'Long', False),
     ('Half', 'Half', 'Double', True),
+    ('BFloat16', 'BFloat16', 'BFloat16AccrealNotDefined', True),
 ]
 
 