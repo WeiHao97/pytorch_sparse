xicun/pytorch

Add a bitwise NOT operator for integer and Boolean types (CPU).
Related to #20024, #22246, #22262

gh-metadata: pytorch pytorch 22283 gh/xuhdev/6/head

torch/_torch_docs.py
@@ -704,6 +704,22 @@ Example::
     tensor([0.0000, 0.0000, 0.0000, 1.0000, 1.0000, 0.0000, 0.5000])
 """)
 
+add_docstr(torch.bitwise_not,
+           r"""
+           abs(input, out=None) -> Tensor
+
+Computes the bitwise NOT of the given :attr:`input` tensor. The input must be of integer or Boolean types.
+
+Args:
+    {input}
+    {out}
+
+Example::
+
+    >>> torch.bitwise_not(torch.tensor([-1, -2, 3], dtype=torch.int8))
+    tensor([ 0,  1, -4], dtype=torch.int8)
+""")
+
 add_docstr(torch.bmm,
            r"""
 bmm(batch1, batch2, out=None) -> Tensor

WeiHao97/pytorch_sparse

torch/_torch_docs.py
@@ -704,6 +704,22 @@ Example::
     tensor([0.0000, 0.0000, 0.0000, 1.0000, 1.0000, 0.0000, 0.5000])
 """)
 
+add_docstr(torch.bitwise_not,
+           r"""
+           bitwise_not(input, out=None) -> Tensor
+
+Computes the bitwise NOT of the given :attr:`input` tensor. The input must be of integer or Boolean types.
+
+Args:
+    {input}
+    {out}
+
+Example::
+
+    >>> torch.bitwise_not(torch.tensor([-1, -2, 3], dtype=torch.int8))
+    tensor([ 0,  1, -4], dtype=torch.int8)
+""")
+
 add_docstr(torch.bmm,
            r"""
 bmm(batch1, batch2, out=None) -> Tensor