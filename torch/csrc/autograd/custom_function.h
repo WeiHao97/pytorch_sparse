190b320e33ac4c637f74a733fca2c3b472e937e8
mal <mal@whatsapp.com>
2019-07-22 12:13:48 -0400

    Support custom autograd functions in C++
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/23020
    ghstack-source-id: 684815f45320282c9f73785d05073841f92f7412

--- torch/csrc/autograd/custom_function.h
+++ torch/csrc/autograd/custom_function.h
@@ -11,4 +11,49 @@ TORCH_API variable_list _wrap_outputs(
   const std::unordered_set<at::TensorImpl*> &dirty_inputs,
   const at::ArrayRef<Variable> raw_outputs,
   const std::shared_ptr<Function> &cdata);
+
+// To use custom autograd operations implement a CFunction subclass with
+// static backward and forward functions
+//
+// forward() can take as many arguments as you want and should return a
+// variable list. Use of any direct Variable arguments will be registered in
+// the graph but no vectors/sets or any other data structures will be traversed.
+//
+// backward() will be given a variable list containing as many Variables as
+// there were outputs from forward. It should return as many Variables as there
+// were inputs with each of them containing the gradient w.r.t. its
+// corresponding input
+//
+// For example:
+// class MyFunction : public CFunction<MyFunction> {
+//   public:
+//   static variable_list forward(AutogradContext *ctx, int n, Variable var);
+//
+//   static variable_list backward(AutogradContext *ctx, variable_list grad_output);
+// };
+// To use MyFunction
+// Variable x;
+// MyFunction::apply(6, x);
+
+template <class T>
+struct CFunction {
+  template<typename... Args>
+  static variable_list apply(Args&&... args);
+};
+
+struct AutogradContext {
+  std::vector<torch::autograd::SavedVariable> saved_variables;
+
+  std::unordered_set<at::TensorImpl*> non_differentiable;
+  std::unordered_set<at::TensorImpl*> dirty_inputs;
+};
+
+template <class T>
+struct CustomFunc : public Function {
+
+  variable_list apply(variable_list&& inputs) override;
+  AutogradContext ctx;
+
+  void release_variables() override;
+};
 }} // namespace torch::autograd
