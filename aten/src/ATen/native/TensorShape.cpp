8ecf71ce6bfb272461d88fbcfd8f70a6bc12ec9a
Jerry Zhang <jerryzh168@gmail.com>
2019-07-23 15:11:27 -0700

    [fix] at::view
    accidently calls clone, but what we want is creating an empty tensor and set storage.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/23239
    ghstack-source-id: 87043230
    
    Differential Revision: [D16442756](https://our.internmc.facebook.com/intern/diff/D16442756/)

--- aten/src/ATen/native/TensorShape.cpp
+++ aten/src/ATen/native/TensorShape.cpp
@@ -909,9 +909,21 @@ Tensor view(const Tensor& self, IntArrayRef size) {
     "not compatible with input tensor's size and stride (at least one dimension"
     " spans across two contiguous subspaces). Use .reshape(...) instead.");
   auto stride_value = *stride;
-  auto self_ = self.clone();
-  self_.set_(self.storage(), self.storage_offset(), inferred_size,
-             stride_value);
+  Tensor self_;
+  if (self.is_quantized()) {
+    TORCH_CHECK(self.qscheme() == kPerTensorAffine,
+                "Only PerTensorAffine quantization is supported right now");
+    auto impl = c10::make_intrusive<QTensorImpl>(Storage(self.storage()), self.type_id(),
+                    get_qtensorimpl(self)->quantizer());
+    impl->set_storage_offset(self.storage_offset());
+    impl->set_sizes_and_strides(inferred_size, stride_value);
+    self_ = Tensor(impl);
+  } else {
+    auto impl = c10::make_intrusive<TensorImpl>(Storage(self.storage()), self.type_id());
+    impl->set_storage_offset(self.storage_offset());
+    impl->set_sizes_and_strides(inferred_size, stride_value);
+    self_ = Tensor(impl);
+  }
   return self_;
 }
 
